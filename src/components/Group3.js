18f7dd0cd8f5a2ac28c0c76466bf3c8f72f40c69
Brian Bycynski <high5bri@hotmail.com>
2017-05-05 11:41:16 -0400

    added another group component

---
+++ src/components/Group3.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
+
+const WellStyles = {maxWidth: 400, margin: '0 auto 10px'}
+
+export default class Group3 extends Component {
+  render () {
+    return <div>
+      <div className='well' style={WellStyles}>
+        <a href=''>
+          <Button className='butt' block>Bdog</Button>
+        </a>
+      </div>
+    </div>
+  }
+}
--- src/components/ReactRouter.js
+++ src/components/ReactRouter.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Group1 from './Group1'
 import Group2 from './Group2'
+import Group3 from './Group3'
+
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 const restaurants1 = () => (
@@ -14,6 +16,11 @@ const restaurants2 = () => (
     <Group2 />
   </div>
 )
+const restaurants3 = () => (
+  <div>
+    <Group3 />
+  </div>
+)
 
 const BasicExample = () => (
   <Router>
@@ -21,10 +28,12 @@ const BasicExample = () => (
       <ul>
         <li><Link to='/'><h2>Group 1</h2></Link></li>
         <li><Link to='/restaurants2'><h2>Group 2</h2></Link></li>
+        <li><Link to='/restaurants3'><h2>Group 3</h2></Link></li>
       </ul>
       {/* <hr /> */}
       <Route exact path='/' component={restaurants1} />
       <Route path='/restaurants2' component={restaurants2} />
+      <Route path='/restaurants3' component={restaurants3} />
     </div>
   </Router>
 )
