bae54816afcf8c5e6649f06d0e47c77a7d4b6f48
Brian Bycynski <high5bri@hotmail.com>
2017-05-06 05:54:22 -0400

    added a few restaurants

--- src/components/Group3.js
+++ src/components/Group3.js
@@ -7,8 +7,14 @@ export default class Group3 extends Component {
   render () {
     return <div>
       <div className='well' style={WellStyles}>
-        <a href=''>
-          <Button className='butt' block>Bdog</Button>
+        <a href='http://www.bigapplebuffet.com'>
+          <Button className='butt' block>Big Apple Buffet</Button>
+        </a>
+        <a href='https://www.yelp.com/biz/chi-town-beefs-and-dogs-saint-petersburg'>
+          <Button className='butt' block>Chi Town Beef and Dogs</Button>
+        </a>
+        <a href='https://www.midpeninsulaseafood.com/'>
+          <Button className='butt' block>Mid Peninsula Seafood</Button>
         </a>
       </div>
     </div>
