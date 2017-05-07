07d1e35d14e172c15f5a8fb37395710de101e0a4
Brian Bycynski <high5bri@hotmail.com>
2017-05-07 06:39:04 -0400

    added another restaurant

--- src/components/Group3.js
+++ src/components/Group3.js
@@ -13,11 +13,20 @@ export default class Group3 extends Component {
         <a href='https://www.yelp.com/biz/chi-town-beefs-and-dogs-saint-petersburg'>
           <Button className='butt' block>Chi Town Beef and Dogs</Button>
         </a>
-        <a href='https://www.midpeninsulaseafood.com/'>
+        <a href='https://www.midpeninsulaseafood.com'>
           <Button className='butt' block>Mid Peninsula Seafood</Button>
         </a>
-        <a href='http://www.mazzarosmarket.com/'>
+        <a href='http://www.mazzarosmarket.com'>
           <Button className='butt' block>Mazzaro's Italian Market</Button>
+        </a>
+        <a href='http://www.cicciocali.com'>
+          <Button className='butt' block>Ciccio's Cali</Button>
+        </a>
+        <a href='https://metrodiner.com/'>
+          <Button className='butt' block>Metro Diner</Button>
+        </a>
+        <a href='http://www.senortacomexicanrestaurant.com'>
+          <Button className='butt' block>Senor Taco</Button>
         </a>
       </div>
     </div>
