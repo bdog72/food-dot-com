0678fc9f8bb771d9cb86ad85f64835e31965325d
Brian Bycynski <high5bri@hotmail.com>
2017-05-08 05:41:56 -0400

    added a new restaurant

--- src/components/ReactRouter.js
+++ src/components/ReactRouter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Group1 from './Group1'
 import Group2 from './Group2'
 import Group3 from './Group3'
+import Group4 from './Group4'
 import { Jumbotron } from 'react-bootstrap'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
@@ -22,14 +23,21 @@ const restaurants3 = () => (
   </div>
 )
 
+const restaurants4 = () => (
+  <div>
+    <Group4 />
+  </div>
+)
+
 const BasicExample = () => (
   <Router>
     <div>
       <Jumbotron>
         <ul>
-          <li><Link to='/'><h2>Group 1</h2></Link></li>
-          <li><Link to='/restaurants2'><h2>Group 2</h2></Link></li>
-          <li><Link to='/restaurants3'><h2>Group 3</h2></Link></li>
+          <li><Link to='/'><h2>Group<br />1</h2></Link></li>
+          <li><Link to='/restaurants2'><h2>Group<br />2</h2></Link></li>
+          <li><Link to='/restaurants3'><h2>Group<br />3</h2></Link></li>
+          <li><Link to='/restaurants4'><h2>Group<br />4</h2></Link></li>
         </ul>
       </Jumbotron>
 
@@ -37,6 +45,7 @@ const BasicExample = () => (
       <Route exact path='/' component={restaurants1} />
       <Route path='/restaurants2' component={restaurants2} />
       <Route path='/restaurants3' component={restaurants3} />
+      <Route path='/restaurants4' component={restaurants4} />
     </div>
   </Router>
 )
