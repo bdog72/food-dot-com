2195402ca0f15a696cb0272cb5d93880556e638d
Brian Bycynski <high5bri@hotmail.com>
2017-05-17 05:53:54 -0400

    minor changes

--- src/components/Group4.js
+++ src/components/Group4.js
@@ -7,8 +7,11 @@ export default class Group4 extends Component {
   render () {
     return <div>
       <div className='well' style={WellStyles}>
-        <a href='http://www.blakescrabcakes.com/'>
+        <a href='http://www.blakescrabcakes.com'>
           <Button className='butt' block>Blakes Crab Cake</Button>
+        </a>
+        <a href='http://www.docksidedavesgrill.com'>
+          <Button className='butt' block>Dave's Dockside</Button>
         </a>
       </div>
     </div>
