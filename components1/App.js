9a7a6e5eb32b9c2169efbd8cd11ca3154c782a32
Brian Bycynski <high5bri@hotmail.com>
2017-05-18 06:53:23 -0400

    started new project

--- components1/App.js
+++ components1/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import ReactRouter from './ReactRouter'
 
 export default class App extends Component {
   render () {
     return <div>
-      Bdog
+      <h1>Restaurants</h1>
+      <ReactRouter />
     </div>
   }
 }
