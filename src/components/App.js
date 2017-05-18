39da741d5ad45dfcb2e62d860018e23945fe98f7
Brian Bycynski <high5bri@hotmail.com>
2017-05-18 08:58:46 -0400

    put in a headliner

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render () {
     return <div>
-      Bdog
+      <h1 className='header'>Restaurants</h1>
     </div>
   }
 }
