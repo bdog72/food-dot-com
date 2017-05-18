52ff2b1b9cea75cc05b4ef89eac73908399c3f77
Brian Bycynski <high5bri@hotmail.com>
2017-05-18 06:19:31 -0400

    made some css changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,6 @@ export default class App extends Component {
   render () {
     return <div>
       <h1>Restaurants</h1>
-      {/* <h1>I</h1>
-      <h1>Enjoy</h1> */}
       <ReactRouter />
     </div>
   }
