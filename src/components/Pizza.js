6e93a427bc3d96d3f2d7dc7a574deb6743000c7f
Brian Bycynski <high5bri@hotmail.com>
2017-05-20 06:06:44 -0400

    added some new css

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
-
+import { Button } from 'react-bootstrap'
 export default class Pizza extends Component {
   render () {
     return <div>
-      Bdog
+      <div className='flex'>
+        <a href='https://tobysoriginallittleitalypizza.com'>
+          <Button className='pizza'>Little Italy</Button>
+        </a>
+        <a href='http://laitaliamia.com'>
+          <Button className='pizza'>Italia Mia</Button>
+        </a>
+        <a href='http://paisanospnp.com'>
+          <Button className='pizza'>Paisanos</Button>
+        </a>
+        <a href='http://www.28gramspizza.com'>
+          <Button className='pizza'>28 Grams Pizza</Button>
+        </a>
+      </div>
     </div>
   }
 }
