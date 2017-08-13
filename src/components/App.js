f3fc434bfa1e5fb81d197fb5ca4a49718f09149b
Brian Bycynski <high5bri@hotmail.com>
2017-08-13 06:06:51 -0400

    changed some font color

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import ReactRouter from './ReactRouter'
+import { PageHeader } from 'react-bootstrap'
 export default class App extends Component {
   render () {
     return <div>
-      <h1 className='header'>Restaurants</h1>
-      <h1 className='header'>That I Enjoy</h1>
+      <PageHeader className='header1'>
+        <h1 className='header'>Restaurants</h1>
+        <h1 className='header'>That I Enjoy</h1>
+      </PageHeader>
       <div>
         <ReactRouter />
       </div>
